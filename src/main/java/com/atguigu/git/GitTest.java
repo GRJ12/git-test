cac1f98f7f354e591372751c654ae1affbddd997
Grj <871750798@qq.com>
2022-08-23 10:34:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author
+ * @create 2022-08-23 上午10:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
